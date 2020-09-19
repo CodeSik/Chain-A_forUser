7cb5d251e7788f064cfadfe3073f664ff688c926
s0metimes <tlghks8712@daum.net>
2020-09-20 02:05:24 +0900

    서버로 Request 하는것 구현

---
+++ app/src/main/java/map/RecordResponse.java
@@ -0,0 +1,56 @@
+package map;
+
+import com.google.gson.annotations.SerializedName;
+
+public class RecordResponse {
+    @SerializedName("lat")
+    private String lat;
+    @SerializedName("lng")
+    private String lng;
+    @SerializedName("companyName")
+    private String companyName;
+    @SerializedName("addressName")
+    private String addressName;
+    @SerializedName("timestamp")
+    private String time;
+
+    public String getLat() {
+        return lat;
+    }
+
+    public void setLat(String lat) {
+        this.lat = lat;
+    }
+
+    public String getLng() {
+        return lng;
+    }
+
+    public void setLng(String lng) {
+        this.lng = lng;
+    }
+
+    public String getCompanyName() {
+        return companyName;
+    }
+
+    public void setCompanyName(String companyName) {
+        this.companyName = companyName;
+    }
+
+    public String getAddressName() {
+        return addressName;
+    }
+
+    public void setAddressName(String addressName) {
+        this.addressName = addressName;
+    }
+
+    public String getTime() {
+        return time;
+    }
+
+    public void setTime(String time) {
+        this.time = time;
+    }
+}
